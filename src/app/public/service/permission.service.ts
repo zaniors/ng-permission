05635a38a5118a6433525a2e4164ed80a1bc4a89
zhangxu <2590000714@qq.com>
2018-10-30 00:27:07 +0800

    权限全局服务

---
+++ src/app/public/service/permission.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { PermissionFactory } from 'src/app/permissions/permissons-factory';
+import { PermissionType } from '../enum/permission-type.enum';
+import { Role } from '../enum/role.enum';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PermissionService {
+    constructor() {}
+
+    /**
+     * 判断当前角色是否有此权限，有此权限则为true，否则false
+     * @param permission 传入权限类型
+     */
+    isGranted(permission: PermissionType): boolean {
+        const permissions = PermissionFactory.getInstance().permissions;
+
+        permissions.forEach(perm => {
+            if (perm === permission) {
+                return true;
+            }
+        });
+        return false;
+    }
+
+    /**
+     * 定义角色类型，保存到localstorage中
+     * @param role 设置当前用户角色类型
+     */
+    authAs(role: Role) {
+        role = role === null ? Role.UNKNOWN : role;
+        localStorage.setItem('role', role);
+    }
+}
