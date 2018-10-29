5d146877954bd9e1721b0e8744e9acef7376a959
zhangxu <2590000714@qq.com>
2018-10-29 21:37:47 +0800

    define permission type

---
+++ src/app/public/enum/permission-type.enum.ts
@@ -0,0 +1,13 @@
+/** 定义权限的类型 */
+export enum PermissionType {
+    /** 可创建 */
+    CREATE = 'CREATE',
+    /** 可读 */
+    READ = 'READ',
+    /** 可更新 */
+    UPDATE = 'UPDATE',
+    /** 可删 */
+    DELETE = 'DELETE',
+    /** 其它权限 */
+    OTHER = 'OTHER'
+}
