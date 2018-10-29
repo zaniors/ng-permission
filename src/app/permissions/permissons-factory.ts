5c8a6c7375e13fa6a704aea54f973171c5c7549a
zhangxu <2590000714@qq.com>
2018-10-30 00:25:11 +0800

    build permission factory

---
+++ src/app/permissions/permissons-factory.ts
@@ -0,0 +1,35 @@
+import { PermissionBase } from '../public/base/permission.base';
+import { Role } from '../public/enum/role.enum';
+import { SuperAdminPermission } from './super-admin.permission';
+import { AdminPermission } from './admin.permission';
+import { UnknownPermission } from './unknown.permission';
+import { UserPermission } from './user.permission';
+export class PermissionFactory {
+    static instance: PermissionBase;
+    constructor() { }
+
+    public static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+
+        const role = localStorage.getItem('role');
+        switch (role) {
+            case Role.SUPERADMIN:
+                this.instance = new SuperAdminPermission();
+                break;
+            case Role.ADMIN:
+                this.instance = new AdminPermission();
+                break;
+            case Role.USER:
+                this.instance = new UserPermission();
+                break;
+            case Role.UNKNOWN:
+                this.instance = new UnknownPermission();
+                break;
+            default:
+                this.instance = new UnknownPermission();
+                break;
+        }
+    }
+}
