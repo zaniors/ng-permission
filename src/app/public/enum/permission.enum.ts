b28a8b68242a2b79bef74a04498388c86b9bc568
zhangxu <2590000714@qq.com>
2018-10-29 21:29:13 +0800

    define role type

---
+++ src/app/public/enum/permission.enum.ts
@@ -0,0 +1,11 @@
+/** 定义角色类型 */
+enum Role {
+    /** 超级管理员 */
+    SUPERADMIN = 'sudo',
+    /** 管理员 */
+    ADMIN = 'admin',
+    /** 普通用户 */
+    USER = 'user',
+    /** 未知用户 */
+    UNKNOWN = 'unknown'
+}
